b181d8c08e9ab99b8aff997831b2753114a005ac
nadiagoralski <naadialynn@gmail.com>
2019-02-12 22:29:57 -0500

    Updated some comments

--- src/transaction.h
+++ src/transaction.h
@@ -16,28 +16,28 @@ public:
     ~Transaction();
 
     /**
-     * @param User; user account to add credit to
-     * @param double; the amount of credit to add
+     * @param User user - user account to add credit to
+     * @param double creditToAdd - the amount of credit to add
     */
     void addCredit(User user, double creditToAdd);
 
     /**
-     * @param User; buyer
-     * @param TicketBatch; the ticket to purchase
-     * @param integer; number of tickets to buy
+     * @param User buyer - buyer of ticket
+     * @param TicketBatch ticketBatch - the ticket to purchase
+     * @param int numberOfTickets -number of tickets to buy
     */
     void buyTicket(User buyer, TicketBatch ticketBatch, int numberOfTickets);
 
     /**
-     * @param User; buyer
-     * @param User; seller
-     * @param double; credit transfered between buyer and seller
+     * @param User buyer - user to refund credit to
+     * @param User seller - user to refund credit from
+     * @param double creditAmount - credit transfered between buyer and seller
     */
     void refund(User buyer, User seller, double creditAmount);
 
     /**
-     * @param User; user account to remove credit fro
-     * @param double; the amount of credit to remove
+     * @param User user - user account to remove credit from
+     * @param double creditToSubtract - the amount of credit to remove
     */
     void removeCredit(User user, double creditToSubtract);
 
